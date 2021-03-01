309f71bac9a720cc660c9a34dbd0795b0bd5b124
unknown <barwy@live.com>
2021-03-01 15:12:42 +0100

    Added notes to the main file and started working on homework6

--- SeleniumInJavaCourse/3.CSSSelectors/src/test/java/CSSSelectors.java
+++ SeleniumInJavaCourse/3.CSSSelectors/src/test/java/CSSSelectors.java
@@ -28,3 +28,44 @@ public class CSSSelectors {
         driver.findElement(By.cssSelector("[id=twotabsearchtextbox]"));
     }
 }
+
+//selektor można wyszukać w kosoli przeglądarki wpisując $$("szukany_selektor")
+
+/*
+id i szukanie w zakładce elements:
+#id -> #twotabsearchtextbox;
+typ elementu i jego id -> input#twotabsearchtextbox;
+input[id='twotabsearchtextbox']
+*/
+
+/*
+class i szukanie w zakładce elements:
+. i nazwa klasy -> .a-cardui-body -> znajdowane są wszystkie elementy zawierające tę klasę;
+tag i nazwa klasy -> div.a-cardui-body;
+tagi o danej klasie -> div[class='a-cardui-body'];
+*/
+
+/*
+Szukanie elementów z 2 klasami (zakładka elements):
+wyszukiwany jest dokładnie taki zestaw klas -> [class='a-image-container a-dynamic-image-container'];
+z nazwą taga -> div[class='a-image-container a-dynamic-image-container'];
+*/
+
+
+/*
+Szukanie po atrybucie: [alt='wartość atrybutu']
+Lączenie atrybutów (kolejność nie ma znaczenia):
+[class='jakaś-klasa'][height='wartość atrybutu']
+.jakaś-klasa[height='wartość atrybutu']
+#jakieś-id[title='wartość atrybutu']
+#jakieś-id.kajaś-klasa
+
+driver.findElement(By.cssSelector("li[class='cat-item cat-item-18']"));
+li - tag;
+class - atrybut;
+
+'cat-item cat-item-18' - wartości atrybutu 1 i 2;
+
+a[aria-label='0 items in cart'][tabindex='65']#nav-cart -> przykład z amazon.com
+na początku znajduje sie tag "a" mówiący jakiego typu jest to obiekt (tag nie jest obowiązkowy).
+*/
---
+++ SeleniumInJavaCourse/3.CSSSelectors/src/test/java/Homework6_CSSSelectors1.java
@@ -0,0 +1,49 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/*
+Zlokalizuj za pomocą lokatorów CSS:
+1. +okienko użytkownik/email;
+2. +okienko hasło;
+3. okienko adres email;
+4. okienko hasło
+5. przyciks zaloguj;
+6. checkbox zapamietaj;
+7. link "nie pamiętasz hasła?";
+8. przycisk zarejestruj się;
+9. link do kategorii "Wspinaczka"
+ */
+
+public class Homework6_CSSSelectors1 {
+    WebDriver driver;
+
+    @BeforeEach
+    public void testStart() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().setSize(new Dimension(800, 600));
+        driver.get("https://fakestore.testelka.pl/moje-konto/");
+    }
+
+    @AfterEach
+    public void testEnd() {
+        driver.close();
+        driver.quit();
+    }
+
+    @Test
+    public void CSSTest(){
+        driver.findElement(By.cssSelector("input[name='username']"));
+        driver.findElement(By.cssSelector("input[name='password']"));
+        driver.findElement(By.cssSelector("input[id='reg_email'][name='email']"));
+        driver.findElement(By.cssSelector("li[class='cat-item cat-item-18']"));
+        driver.findElement(By.cssSelector("label[for='reg_email']"));
+
+
+    }
+}
